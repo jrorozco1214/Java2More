7e9ac251ae82328456c3796c3e6ec2e09eacab15
Jeremy <jeremyorozco38@gmail.com>
2025-03-19 20:03:23 -0500

    File Creation

--- CreatingFile.java
+++ CreatingFile.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class CreatingFile {
@@ -9,19 +10,18 @@ public class CreatingFile {
         File f1 = new File("C:\\Users\\jerem\\OneDrive\\Documents\\GitHub\\Java2More\\input.txt");
         Scanner sc = new Scanner(f1);
 
-        sc.useDelimiter(",\\s"+);
+        sc.useDelimiter("[,\\s]+");
+
+        PrintWriter p1 = new PrintWriter("C:\\Users\\jerem\\OneDrive\\Documents\\GitHub\\Java2More\\output.txt");
 
         while(sc.hasNext()) {
-
+            p1.println(sc.next());
         }
-    
-    
+        sc.close();
+        p1.close();
     }
-
      catch(IOException e){
         e.getMessage();
      }
-        
-
     }
 }
--- SplitMethod.java
+++ SplitMethod.java
@@ -1,4 +1,4 @@
-public class Practice {
+public class SplitMethod {
 public static void main(String[] args) {
     
     String s1 = "8,9,9,8,5";
@@ -13,10 +13,6 @@ public static void main(String[] args) {
 
 
     System.out.println(total);
-
-
-
-
 
     } 
 }
