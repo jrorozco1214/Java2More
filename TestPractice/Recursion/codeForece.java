3c4eb3a6d5a1f36f81b81cdd5e1e166a16e42f3d
Jeremy <jeremyorozco38@gmail.com>
2025-04-07 10:33:24 -0500

    code force

--- TestPractice/Recursion/codeForece.java
+++ TestPractice/Recursion/codeForece.java
@@ -7,12 +7,31 @@ public class codeForece {
         Scanner sc = new Scanner(System.in);
 
         int num = sc.nextInt();
+        int []constant = {0, 1, 0, 3, 2, 0, 2, 5};
 
         for(int i = 0; i < num; i++){
         int secondIn = sc.nextInt();
 
+            int []ar = new int [10];
             for(int j = 0; j < secondIn; j++){
-                System.out.print((int)Math.random() * 9 + " ");
+            int thirdIn = sc.nextInt();
+                ar[j] = thirdIn; 
+            }
+            System.out.println();
+            int counter = 0;
+            
+            for(int z = 0; z < constant.length; z++){
+                for(int x = 0; x < ar.length; x++){
+                    if(constant[z] == ar[x]){
+                        ar[x] = -1;
+                        counter = x;
+                    }
+                }
+            }
+            if(counter == 0){
+                System.out.println(0);
+            } else {
+                System.out.println(counter+1);
             }
         }
         sc.close();
