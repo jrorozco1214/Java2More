a284d17b619a7342832b5e79e37736139414f1e0
Jeremy <jeremyorozco38@gmail.com>
2025-09-12 10:54:02 -0500

    chagnes

---
+++ RockPaperScissors.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class RockPaperScissors {
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Rock Paper Scissors");
+        System.out.println("Please enter in 1 = Rock 2 = Paper or 3 = Scissors");
+        
+        String input = sc.next();
+
+        int changed_to_integer = Integer.parseInt(input);
+
+
+
+    }
+    
+}
---
+++ idealGenerator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class idealGenerator {
+
+    public static void main(String[] args) {
+        
+        //n(n>=k)
+        //k is the lenght of the array
+        //n need to to be more then k
+        //if k is 2 a[x, x] etc
+        //given vlaue of k is (1 < k < 1000)
+        //n is the sum, if it's odd then the sum has to be n>=3, for odd the middle can add up
+        //n>=k
+        //if you have an even k and an odd 
+
+        //s
+
+        Scanner sc = new Scanner(System.in);
+
+        
+
+
+
+
+
+    }
+    
+}
