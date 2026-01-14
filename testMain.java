8e480143f6db2e1972d8e87bdabced783f258b98
Jeremy <jeremyorozco38@gmail.com>
2026-01-14 10:44:21 -0600

    changes

--- testMain.java
+++ testMain.java
@@ -2,11 +2,14 @@ import java.util.ArrayList;
 
 public class testMain {
     public static void main(String[] args) {
-    
-        int []ar = new int[3];
 
-        for(int x: ar){
-            System.out.println(x);
-        }
     }
+        public static int fact(int n){
+	        if(n == 0){
+                System.out.println("hi");
+	        }
+	        int x = 1;
+	        return x * fact(n-1);
+	}
 }
+                                         
