0c609662f38b7bb21de5138b5dac360546b045e7
Jeremy <jeremyorozco38@gmail.com>
2026-02-24 00:06:36 -0600

    Changes

--- OperatingSystemsProject/opmain.java
+++ OperatingSystemsProject/opmain.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Scanner;
 import java.io.File;
@@ -5,39 +7,20 @@ import java.io.FileNotFoundException;
 
 public class opmain {
 
-    public static class Processes {
-        private int process = 0;
-        private int cpu, disk, tty, deadlock, processorTime;
+    public static class Processes implements Comparable<Processes>{
+        private String processNames;
+        private int cpu, disk, tty, deadline, processorNumber, processorArrivalTime;
 
-        // public Processes(int cpu, int disk, int tty, int deadlock){
-        //     if(cpu == 0){
-        //         this.cpu = 0;
-        //     } else if(disk == 0){
-        //         this.
-        //     }
-            
-        //     this.cpu = cpu;
-        //     this.disk = disk;
-        //     this.tty = tty;
-        //     this.deadlock = deadlock;
-        //     process +=1;
-        // }
+        public void setProcessNames(String processNames){
+            this.processNames = processNames;
+        }
 
-        // public Processes(int cpu, int disk, int tty){
-        //     this.cpu = cpu;
-        //     this.disk = disk;
-        //     this.tty = tty;
-        //     process += 1;
-        // }
+        public void setProcessorNumber(int processorNumber){
+            this.processorNumber = processorNumber;
+        }
 
-        // public Processes(int cpu, int disk){
-        //     this.cpu = cpu;
-        //     this.disk = disk;
-        //     process += 1;
-        // }
-
-        public void setProcessorTime(int processorTime){
-            this.processorTime = processorTime;
+        public void setProcessorArrivalTime(int processorArrivalTime){
+            this.processorArrivalTime = processorArrivalTime;
         }
 
         public void setCpu(int cpu){
@@ -52,54 +35,95 @@ public class opmain {
             this.tty = tty;
         }
 
-        public void setDeadLock(int deadlock){
-            this.deadlock = deadlock;
+        public void setDeadLine(int deadline){
+            this.deadline = deadline;
+        }
+
+        @Override
+        public int compareTo(opmain.Processes o) {
+            return -1;
         }
     } 
-
-
+    
+    static Queue<Processes> interactiveQueue = new PriorityQueue<>();
+    static Queue<Processes> realtimeQueues = new PriorityQueue<>();
+    static Queue<Processes> diskQueues = new PriorityQueue<>();
 
     public static void main(String[] args) throws FileNotFoundException {
+        
+        File f1 = new File("C:\\Users\\jerem\\Documents\\GitHub\\Java2More\\OperatingSystemsProject\\test1.txt");
 
-        Scanner read = new Scanner(new File("input.txt"));
+        Scanner read1 = new Scanner(f1);
 
-        read.useDelimiter("\\n");
-
-        String content = read.next();
+        String content1 = read1.next();
 
         int tracker = 0;
 
-        Queue<Processes> queues = null;
+        Queue<Processes> queues = new PriorityQueue<>();
 
         int numberOfProcesses = 0;
 
-        while(read.hasNext()){
-            if(content == "INTERACTIVE" || content == "REAL-TIME"){
+        while(read1.hasNext()){
+            if(content1.equals("INTERACTIVE") || content1.equals("REAL-TIME")){
                 numberOfProcesses++;
             }
+            System.out.println(content1);
+            content1 = read1.next();
         }
 
-        for(int i = 0; i < numberOfProcesses; i++){
-                if(content == "INTERACTIVE" || content == "REAL-TIME"){
+        Scanner read2 = new Scanner(f1);
 
-                while(read.hasNext()){
-                    String reading = read.next();
-                    int value = read.nextInt();
+        String content2 = read2.next();
+
+        for(int i = 0; i < numberOfProcesses; i++){
+                if(content2.equals("INTERACTIVE") || content2.equals("REAL-TIME")){
                     Processes p = new Processes();
 
-                    if(reading == "CPU" || reading == "DISK" || reading == "TTY" || reading == "DEADLOCK"){
-                        p.setCpu(value);
-                        p.setDisk(value);
-                        p.setTTY(value);
-                        p.setDeadLock(value);
-                    } else if(reading == "INTERACTIVE" || reading == "REAL-TIME"){
-                        tracker++;
-                        p.setProcessorTime(tracker);
-                        break;
+                    p.setProcessNames(content2);
+                    content2 = read2.nextLine().trim();
+
+                    p.setProcessorArrivalTime(Integer.parseInt(content2));
+                    p.setProcessorNumber(++tracker);
+
+                    while(read2.hasNextLine()){
+
+                        content2 = read2.next();
+
+                        int value = Integer.parseInt(read2.nextLine().trim());
+
+                        switch (content2) {
+                            case "CPU":
+                                p.setCpu(value);
+                                continue;
+                        
+                            case "DISK":
+                                p.setDisk(value);
+                                continue;
+                        
+                            case "TTY":
+                                p.setTTY(value);
+                                continue;
+                        
+                            case "DEADLINE":
+                                p.setDeadLine(value);
+                                continue;
                     }
-                queues.add(p);
                 }
+                queues.add(p);
             }
+        }
+        read1.close();
+        read2.close();
+    }
+
+    public static void cpuSimulation(int numberOfProcesses){
+
+        int size = numberOfProcesses;
+
+        HashSet<Processes> p = new HashSet<>();
+
+        while(p.size() < size){
+
         }
     }
 }
