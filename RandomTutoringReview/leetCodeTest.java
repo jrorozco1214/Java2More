2a34bd94359030d69362b7a0b43bd5c363d003fa
Jeremy <jeremyorozco38@gmail.com>
2026-05-22 22:57:17 -0500

    changes

--- RandomTutoringReview/leetCodeTest.java
+++ RandomTutoringReview/leetCodeTest.java
@@ -11,40 +11,52 @@ public class leetCodeTest {
     
     public static void main(String[] args) {
 
-        int[] nums1 = {-1,0,0,3,3,3,0,0,0};
-        int[] nums2 = {1,2,2};
-        int m = 6;
-        int n = 3;
+        // int[] nums1 = {-1,0,0,3,3,3,0,0,0};
+        // int[] nums2 = {1,2,2};
+        // int m = 6;
+        // int n = 3;
 
-        System.out.println(Arrays.toString(merge(nums1, m, nums2, n)));
-    }
+        // System.out.println(Arrays.toString(merge(nums1, m, nums2, n)));
 
-    public static int[] merge(int[] nums1, int m, int[] nums2, int n) {
+        int n = 5;
+        int x = 0;
+        int y = 1;
 
-        int index = 0;
-
-        if(m == 0 && n == 0){
-            System.out.println(nums1);
-        } else if (m > 0 && n == 0){
-            System.out.println(nums1);
-        } else if(m == 0 && n > 0){
-
-            for(int i = 0; i < nums2.length; i++){
-                nums1[i] = nums2[i];
-            }
-        } else {
-
-            for(int j = 0; j < nums1.length; j++){
-
-                if(nums1[j] == 0 && index < nums2.length){
-
-                    nums1[j] = nums2[index];
-                    index++;
-                }
-            }
+        for(int i = 0; i < n; i++){
+            int z = x + y;
+            x = y;
+            y = z;
         }
-        Arrays.sort(nums1);
 
-        return nums1;
+        System.out.println(y);
     }
+
+    // public static int[] merge(int[] nums1, int m, int[] nums2, int n) {
+
+    //     int index = 0;
+
+    //     if(m == 0 && n == 0){
+    //         System.out.println(nums1);
+    //     } else if (m > 0 && n == 0){
+    //         System.out.println(nums1);
+    //     } else if(m == 0 && n > 0){
+
+    //         for(int i = 0; i < nums2.length; i++){
+    //             nums1[i] = nums2[i];
+    //         }
+    //     } else {
+
+    //         for(int j = 0; j < nums1.length; j++){
+
+    //             if(nums1[j] == 0 && index < nums2.length){
+
+    //                 nums1[j] = nums2[index];
+    //                 index++;
+    //             }
+    //         }
+    //     }
+    //     Arrays.sort(nums1);
+
+    //     return nums1;
+    // }
 }
