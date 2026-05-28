178e1226df2a1d5edd97d63900c261c244f89af7
Jeremy <jeremyorozco38@gmail.com>
2026-05-28 15:20:22 -0500

    changes

---
+++ RandomTutoringReview/courseSchedule209.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class courseSchedule209 {
+    public static void main(String[] args) {
+
+        System.out.println(canFinish(4, new int[][] {{3,1},{2,3},{0,2},{1,0}}));
+    }
+
+    public static boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        HashMap<Integer, ArrayList<Integer>> adjList = new HashMap<>(); 
+
+        int[] numDeps = new int[numCourses];
+
+        for(int i = 0; i < prerequisites.length; i++){
+
+            int blocked = prerequisites[i][0];
+            int prereq = prerequisites[i][1];
+            
+            numDeps[blocked]++;
+
+            if(!adjList.containsKey(prereq)){
+
+                ArrayList<Integer> temp = new ArrayList<>();
+                temp.add(blocked);
+                adjList.put(prereq, temp);
+            } else {
+
+                adjList.get(prereq).add(blocked);
+            }
+        }
+
+        Queue<Integer> potCourses = new LinkedList<Integer>();
+
+        for(int i = 0; i < numDeps.length; i++){
+
+            if(numDeps[i] == 0){
+
+                potCourses.add(i);
+            }
+        }
+
+        int counter = 0;
+
+        while(!potCourses.isEmpty()) {
+
+            int head = potCourses.remove();
+            counter++;
+
+            if(!adjList.containsKey(head)){
+                continue;
+            }
+
+            ArrayList<Integer> blockedList = adjList.get(head);
+
+            for(Integer id: blockedList){
+
+                numDeps[id]--;
+
+                if(numDeps[id] == 0){
+                    potCourses.add(id);
+                }
+            }
+        }
+
+        return counter == numCourses;
+    }
+    
+}
---
+++ RandomTutoringReview/groupAnagrams49.java
@@ -0,0 +1,47 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class groupAnagrams49 {
+    public static void main(String[] args) {
+        
+        String[] input = {"eat","tea","tan","ate","nat","bat"};
+
+        System.out.println(groupAnagrams(input));
+
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+
+        char[][] cArray = new char[strs.length][];
+
+        for(int i = 0; i < strs.length; i++){
+
+            cArray[i] = strs[i].toCharArray();
+            
+            Arrays.sort(cArray[i]);
+        }
+
+        HashMap<String, ArrayList<String>> result = new HashMap<>();
+
+        for(int i = 0; i < strs.length; i++){
+
+            String s = new String(cArray[i]);
+
+            if(!result.containsKey(s)) {
+                ArrayList<String> temp = new ArrayList<>();
+                temp.add(strs[i]);
+                result.put(s, temp);
+            } else {
+
+                result.get(s).add(strs[i]);
+            }
+        }
+
+        ArrayList<List<String>> result2 = new ArrayList<>(result.values());
+
+        return result2;
+    }
+}
