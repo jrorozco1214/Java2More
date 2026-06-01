1ec50c84213b9dffac263360e2ed4bf2f3e420c0
Jeremy <jeremyorozco38@gmail.com>
2026-06-01 00:14:29 -0500

    changes

--- RandomTutoringReview/LeetCodeQuestions/pascalTriangle118.java
+++ RandomTutoringReview/LeetCodeQuestions/pascalTriangle118.java
@@ -46,4 +46,25 @@ public class pascalTriangle118 {
 
         return result;
     }
+
+
+    private static int binarySearch(int[] array, int target) {
+		
+		int low = 0;
+		int high = array.length - 1;
+		
+		while(low <= high) {
+			
+			int middle = low + (high - low) / 2;
+			int value = array[middle];
+			
+			System.out.println("middle: " + value);
+			
+			if(value < target) low = middle + 1;
+			else if(value > target) high = middle - 1;
+			else return middle; //target found
+		}
+		
+		return -1;
+	}
 }
