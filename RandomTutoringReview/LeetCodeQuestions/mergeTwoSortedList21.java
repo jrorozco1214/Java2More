6949ac1a1f217d090b10358b798d3012fb94e468
Jeremy <jeremyorozco38@gmail.com>
2026-06-10 10:50:57 -0500

    changes

--- RandomTutoringReview/LeetCodeQuestions/mergeTwoSortedList21.java
+++ RandomTutoringReview/LeetCodeQuestions/mergeTwoSortedList21.java
@@ -26,16 +26,23 @@ public class mergeTwoSortedList21 {
         // v1.next = v2;
         // v2.next = v3;
 
-        // ListNode vv1 = new ListNode(1);
+        // ListNode vv1 = new ListNode(2);
         // ListNode vv2 = new ListNode(3);
         // ListNode vv3 = new ListNode(4);
         // vv1.next = vv2;
         // vv2.next = vv3;
 
-        ListNode v1 = new ListNode();
-        ListNode vv1 = new ListNode(0);
+        ListNode v1 = new ListNode(2);
+        ListNode vv1 = new ListNode(1);
 
         ListNode result = mergeTwoSortedList(v1, vv1);
+
+        ListNode temp = result;
+
+        while(temp != null){
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
 
         System.out.println(result);
     }
@@ -50,13 +57,13 @@ public class mergeTwoSortedList21 {
 
         //check if p1 and p2 are null and return either or 
 
-        if(list1.next == null && list2.next == null) {
+        if(p1 == null && p2 == null) {
 
             return null;
-        } else if (list1.next == null && list2.next != null) {
+        } else if (p1 == null && p2 != null) {
 
             return list2;
-        } else if (list1.next != null && list2.next == null){
+        } else if (p1 != null && p2 == null){
 
             return list1;
         }
@@ -94,15 +101,41 @@ public class mergeTwoSortedList21 {
             } else if(p1.val < p2.val){
 
                 ListNode r3 = new ListNode(p1.val);
-                traverse.next = r3;
-                traverse = traverse.next;
+
+                if(p1.next == null && p2.next == null && head.val == 0){
+                    head.val = p1.val;
+                    head.next = p2;
+                    return head;
+                }
+
+                if(traverse == null) {
+                    traverse = head;
+                    traverse.val = p1.val;
+                    traverse = traverse.next;
+                } else {
+                    traverse.next = r3;
+                    traverse = traverse.next;
+                }
 
                 p1 = p1.next;
             } else if(p1.val > p2.val){
 
                 ListNode r4 = new ListNode(p2.val);
-                traverse.next = r4;
-                traverse = traverse.next;
+
+                if(p1.next == null && p2.next == null && head.val == 0){
+                    head.val = p2.val;
+                    head.next = p1;
+                    return head;
+                }
+
+                if(traverse == null){
+                    traverse = head;
+                    traverse.val = p2.val;
+                    traverse = traverse.next;
+                } else {
+                    traverse.next = r4;
+                    traverse = traverse.next;
+                }
 
                 p2 = p2.next;
             }
