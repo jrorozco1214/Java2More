87391734baa09ce4ba8f9db2e311572fab316d3d
Jeremy <jeremyorozco38@gmail.com>
2026-06-22 10:20:50 -0500

    changes

---
+++ RandomTutoringReview/LeetCodeQuestions/sameTree100.java
@@ -0,0 +1,31 @@
+package LeetCodeQuestions;
+
+
+public class sameTree100 {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val, TreeNode left, TreeNode right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        TreeNode(int val){
+            this.val = val;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        
+        return true;
+    }
+}
